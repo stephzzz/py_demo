228fd8a61f6daecb308c16d08cc2eec8be2f6e67
Stephen <415967043@qq.com>
2017-08-02 01:20:59 +0800

    Update singleton.py

--- OO/singleton.py
+++ OO/singleton.py
@@ -7,18 +7,19 @@
 
 #方法1 使用__new__
 class Singleton(object):
-    __instance = None
+    '''
+    将类的实例和一个类变量 _instance 关联起来，如果 cls._instance 为 None 则创建实例
+    否则直接返回 cls._instance
+    '''
+    _instance = None
+    def __new__(cls, *args, **kw):
+        if not cls._instance:
+            cls._instance = super(Singleton, cls).__new__(cls, *args, **kw)  
+        return cls._instance  
+ 
+class MyClass(Singleton):  
+    a = 1
 
-    def __new__(cls,age,name):
-        if not cls.__instance :
-            cls.__instance = object.__new__(cls)
-        return cls.__instance
-
-a = Singleton(18,'abc')
-b = Singleton(8,'qqq')
-
-print(id(a))
-print(id(b))
 
 ********************************************
 
