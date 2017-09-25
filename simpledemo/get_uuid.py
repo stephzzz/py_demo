5341806a97373c82e496253e69c0e9e4fe5baf2d
Abnerzz <415967043@qq.com>
2017-09-25 15:45:11 +0800

    get uuid

---
+++ simpledemo/get_uuid.py
@@ -0,0 +1,14 @@
+import uuid
+
+
+'''
+uuid:通用唯一识别码
+共5种算法：
+1、在分布式global的分布式计算环境下最好用uuid1
+2、若有名字的唯一性要求最好用uuid3或uuid5
+'''
+def get_uuid():
+    str_uuid = str(uuid.uuid1())
+    return ''.join(str_uuid.split('-'))
+
+print(get_uuid())
